687bd2c69dbfb3cbd35ac5d294e93b68e51e9c66
sahil25123 <sahilgupta25123@gmail.com>
2025-03-04 15:40:45 +0530

    feat: enhance socket connection handling and implement message broadcasting

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -15,6 +15,22 @@ const connectToSocket = (server) =>{
             if(connections[path]=== undefined){
                 connections[path]=[]
             }
+            connections[path].push(Socket.id);
+
+            timeOnline[Socket.id]= new Date();
+            for(let a = 0; a< connections[path].length; a++){
+                io.to(connections[path][a]).emit("user-joined", Socket.id ,connections[path])
+            }
+
+            if(message[path]!== undefined){
+                for(let a = 0;a<message[path].length;a++){
+
+                    io.to(Socket.id).emit("chat-message", message[path][a]["data"],
+                        message[path][a]['sender'],message[path][a]["socked-id-sender"])
+                        
+                }
+
+            }
             
         })
         Socket.on("signal",(toID,message)=>{
