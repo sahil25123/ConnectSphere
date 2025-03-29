30ae7cfe473d2a77ff16b66031ccbc98aef000b6
sahil25123 <sahilgupta25123@gmail.com>
2025-03-29 12:23:31 +0530

    feat: add VideoMeet component with initial structure and server URL configuration

---
+++ frontend/src/pages/VideoMeet.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const  server_url  ="http://localhost:5000/api/v1/users/me";
+export default function VideoMeet() {
+
+
+
+
+    const connections = {};
+
+    // const peerConfigConnection {
+    //     "iceServers": [
+    //         {
+    //             "urls": "stun:stun.l.google.com:19302"
+    //         },
+    //         {
+    //             "urls": "turn:turnserver.example.com",
+    //             "username": "user",
+    //             "credential": "pass"
+    //         }
+    //     ]
+    // }
+  return (
+    <div>
+      <h1>VideoMeet</h1>
+      
+    </div>
+  )
+}
